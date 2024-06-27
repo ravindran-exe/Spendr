8a69286af9c18ee0a7029c32cf9e49571bbbb2f0
ravindran-exe-mac <ravindran2213003@ssn.edu.in>
2024-06-27 20:36:44 +0530

    feat: adding transaction

---
+++ components/AddTransaction.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import addTransaction from "@/actions/addTransaction";
+import { toast } from 'react-toastify';
+import { useRef } from 'react';
+
+const AddTransaction = () => {
+    const formRef = useRef<HTMLFormElement>(null); // clear
+
+    const clientAction = async(formData: FormData) => {
+        const {data, error} = await addTransaction(formData)
+
+        if (error) {
+            toast.error(error);
+          } else {
+            toast.success('Transaction added');
+            formRef.current?.reset();
+          }
+
+    };
+
+    return (
+        <>
+        <h3>Add Transaction</h3>
+        <form ref = {formRef} action={clientAction}>
+            <div className="form-control">
+                <label htmlFor="text">Text</label>
+                <input type="text" id="text" name="text" placeholder="Enter text ..."/>
+            </div>
+            <div className="form-control">
+                <label htmlFor="amount">Amount <br/> (negative - expense, positive - income)</label>
+                <input type="number" id="amount" name="amount" placeholder="Enter amount..." step='0.01'/>
+            </div>
+            <button className="btn">Add Transaction</button>
+        </form>
+
+        </>
+     );
+}
+
+ 
+export default AddTransaction;
