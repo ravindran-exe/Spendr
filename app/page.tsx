a2c5a4af34be0af4b5045be13b0ac740fac9390d
ravindran-exe-mac <ravindran2213003@ssn.edu.in>
2024-06-28 18:12:19 +0530

    feat: final homepage with footer

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,35 @@ import Guest from "@/components/Guest";
 import { currentUser } from "@clerk/nextjs/server";
 import AddTransaction from "@/components/AddTransaction";
 import Balance from "@/components/Balance";
+import IncomeExpense from "@/components/IncomeExpense";
+import TransactionList from "@/components/TransactionList";
 
-const HomePage = async() => {
+const HomePage = async () => {
   const user = await currentUser();
 
-  if(!user){
-    return <Guest />
+  if (!user) {
+    return <Guest />;
   }
-  return ( <main>
-    <h2>Welcome, {user.firstName}</h2>
-    <Balance />
-    <AddTransaction />
-  </main> );
-}
- 
+
+  return (
+    <div className="content-wrapper">
+      <main className="main-content">
+        <h2>Welcome, {user.firstName}</h2>
+        <Balance />
+        <IncomeExpense />
+        <AddTransaction />
+        <TransactionList />
+      </main>
+      <footer className="footer">
+        <div className="footer-container">
+          <span>Made with caffeine ☕︎ | </span>
+          <a href="https://www.linkedin.com/in/ravindran-v-7a3b80248/" className="footer-link">
+            Ravindran
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
 export default HomePage;
