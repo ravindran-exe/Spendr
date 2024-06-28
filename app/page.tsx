75f91683bdd75ba014f9c1b857d94050f72d35a7
ravindran-exe-mac <ravindran2213003@ssn.edu.in>
2024-06-28 09:30:44 +0530

    feat: added balance page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Guest from "@/components/Guest";
 import { currentUser } from "@clerk/nextjs/server";
 import AddTransaction from "@/components/AddTransaction";
+import Balance from "@/components/Balance";
 
 const HomePage = async() => {
   const user = await currentUser();
@@ -9,7 +10,8 @@ const HomePage = async() => {
     return <Guest />
   }
   return ( <main>
-    <h1>Welcome, {user.firstName}</h1>
+    <h2>Welcome, {user.firstName}</h2>
+    <Balance />
     <AddTransaction />
   </main> );
 }
