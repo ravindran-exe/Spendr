6b98d7781885d4349856c4ff9e1c5074bfbcc280
ravindran-exe-mac <ravindran2213003@ssn.edu.in>
2024-06-28 09:33:10 +0530

    feat: get user balance

---
+++ components/Balance.tsx
@@ -0,0 +1,15 @@
+import getUserBalance from '@/actions/getUserBalance';
+import { addCommas } from '@/lib/utils';
+
+const Balance = async () => {
+  const { balance } = await getUserBalance();
+
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>₹{addCommas(Number(balance?.toFixed(2) ?? 0))}</h1>
+    </>
+  );
+};
+
+export default Balance;
